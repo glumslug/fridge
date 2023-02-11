672426b4085618f2edb296eb91e67e6146df94a4
glumslug <glumaslug@gmail.com>
2023-02-11 17:34:00 -0500

    fixed dbhooks build error

--- client/src/utilities/dbHooks.tsx
+++ client/src/utilities/dbHooks.tsx
@@ -1,12 +1,12 @@
 import axios, { AxiosResponse } from "axios";
-import { Items, Users } from "./interfaces";
+import { item, Users } from "./interfaces";
 
 const getAllItems = async () => {
   const response: AxiosResponse = await axios.get(
     "http://localhost:3000/db/get-items"
   );
   if (response.data) {
-    let d: Items[] = response.data;
+    let d: item[] = response.data;
     return d;
   }
 };
@@ -26,7 +26,7 @@ const getItemsByUser = async (id: number) => {
     "http://localhost:3000/db/items-by-user/" + id
   );
   if (response.data) {
-    let d: Items[] = response.data;
+    let d: item[] = response.data;
     return d;
   }
 };

28509010d9b7293ebf79cbcd5b54461faa25fb0d
glumslug <glumaslug@gmail.com>
2023-02-18 16:19:10 -0500

    testing without netlify env

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 const link =
   process.env.NODE_ENV === "development"
     ? "http://127.0.0.1:3000/"
-    : process.env.API_URL;
+    : "http://18.218.156.81:3000/";
 
 // https://vitejs.dev/config/
 export default defineConfig({
